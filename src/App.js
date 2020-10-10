b35d6792874881bc435ed26c56f1353b7d9fed20
rishabh verma <vermarishabh0987@gmail.com>
2020-10-10 15:18:30 +0530

    first chart and hover

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./components/heading/heading";
 import BoxArray from "./components/boxarray/boxarray";
 import "./App.scss";
+import "../node_modules/react-vis/dist/style.css";
+import { XYPlot, LineMarkSeries, XAxis, YAxis } from "react-vis";
+import { matches_played_data, initial_data } from "./temp";
 function App() {
+  const [data, setData] = useState(true); //switch this for animation
+
   return (
     <div className="App">
       <Heading />
       <BoxArray />
+      <div className="bottom-container">
+        <div className="graph">
+          <XYPlot xType="ordinal" height={300} width={600} yDomain={[40, 90]}>
+            <LineMarkSeries
+              animation
+              data={data ? matches_played_data : initial_data}
+              curve={"curveMonotoneX"}
+              style={{
+                strokeWidth: "3px",
+              }}
+              lineStyle={{ stroke: "#e7f6f3" }}
+              markStyle={{ stroke: "#19a58a" }}
+            />
+            <XAxis title="year" />
+            <YAxis title="no. of matches played" />
+          </XYPlot>
+        </div>
+        <div className="news"></div>
+      </div>
     </div>
   );
 }
--- src/components/boxarray/boxarray.js
+++ src/components/boxarray/boxarray.js
@@ -1,37 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./boxarray.scss";
 
 function BoxArray() {
-  const ques = [
+  const [ques, setques] = useState([
     {
+      id: 0,
       title: "Matches Played",
       description: "Know the the number matches played each season..",
       color: "#19a58a",
       lightColor: "#e7f6f3",
       shadow: "0px 0px 69px -15px rgba(25,165,138,1)",
+      selected: true,
     },
     {
+      id: 1,
       title: "By Venue ?",
       description: "How many matches are played at each venue ?",
       color: "#ff8c60",
       lightColor: "#fff4f0",
       shadow: " 0px 0px 69px -15px rgba(255,140,96,1)",
+      selected: false,
     },
     {
+      id: 2,
       title: "In which City !",
       description: "What is the total number of matches played this city ?",
       color: "#fecc8c",
       lightColor: "#fffaf4",
       shadow: "0px 0px 69px -15px rgba(254,204,140,1)",
+      selected: false,
     },
     {
+      id: 3,
       title: "Active Team ?",
       description: "How many matches are played by each team",
       color: "#d07475",
       lightColor: "#fcf0f0",
       shadow: "0px 0px 69px -15px rgba(208,116,117,1)",
+      selected: false,
     },
-  ];
+  ]);
+
+  const changeData = (id) => {
+    if (ques[id].selected) {
+      return;
+    }
+
+    let temp_ques = ques;
+    for (let i = 0; i < temp_ques.length; i++) {
+      if (temp_ques[i].selected) {
+        temp_ques[i].selected = false;
+      }
+    }
+
+    let temp = temp_ques[id];
+    if (!temp.selected) {
+      temp.selected = true;
+    }
+
+    setques([...temp_ques]);
+  };
 
   return (
     <div className="box-array">
@@ -39,11 +67,17 @@ function BoxArray() {
         return (
           <div
             className="box"
-            style={{ backgroundColor: i.color, boxShadow: i.shadow }}
+            key={i.id}
+            onClick={() => changeData(i.id)}
+            style={{ backgroundColor: i.selected ? i.color : "white" }}
           >
-            <h5>{i.title}</h5>
-            <hr></hr>
-            <p>{i.description}</p>
+            <h5 style={{ color: i.selected ? "white" : i.color }}>{i.title}</h5>
+            <hr
+              style={{ backgroundColor: i.selected ? "white" : i.lightColor }}
+            ></hr>
+            <p style={{ color: i.selected ? "white" : i.color }}>
+              {i.description}
+            </p>
           </div>
         );
       })}
