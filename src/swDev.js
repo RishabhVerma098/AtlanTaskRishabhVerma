3d61715834e6e3318aee6282bd0e4b73a60bcaa8
rishabh verma <vermarishabh0987@gmail.com>
2020-10-12 14:12:34 +0530

    name changed

--- src/swDev.js
+++ src/swDev.js
@@ -1,5 +1,5 @@
 export const swDev = () => {
-  let swURL = `${process.env.PUBLIC_URL}/serviceworker.js`;
+  let swURL = `${process.env.PUBLIC_URL}/service-worker.js`;
   navigator.serviceWorker.register(swURL).then((res) => {
     console.log("response", res);
   });
