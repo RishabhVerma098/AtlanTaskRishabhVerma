888c29a893543af1f7e620d356160d04b91f9a8c
rishabh verma <vermarishabh0987@gmail.com>
2020-10-12 13:32:10 +0530

    Updates

---
+++ precache-manifest.8bedd5ae32ea56f577db6d172a306366.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a26ff44624d16e6afd370bc78d952c2",
+    "url": "/AtlanTaskRishabhVerma/index.html"
+  },
+  {
+    "revision": "f45580a31a523a0ac043",
+    "url": "/AtlanTaskRishabhVerma/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "c8c9c53866930d5f77dc",
+    "url": "/AtlanTaskRishabhVerma/static/css/main.cb9ffb5f.chunk.css"
+  },
+  {
+    "revision": "f45580a31a523a0ac043",
+    "url": "/AtlanTaskRishabhVerma/static/js/2.0885b664.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/AtlanTaskRishabhVerma/static/js/2.0885b664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c9c53866930d5f77dc",
+    "url": "/AtlanTaskRishabhVerma/static/js/main.1e30c515.chunk.js"
+  },
+  {
+    "revision": "3277d4f3afa5816fde9a",
+    "url": "/AtlanTaskRishabhVerma/static/js/runtime-main.57c95b75.js"
+  },
+  {
+    "revision": "07f09d74061e5bac5d5f739064d6d87d",
+    "url": "/AtlanTaskRishabhVerma/static/media/logo.07f09d74.png"
+  }
+]);
